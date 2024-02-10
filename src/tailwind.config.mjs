862eba10041c81d975f9f565d9aa3c2b69a7e87f
Luciano Madroñal <lucianoumc1@gmail.com>
2024-02-10 17:06:10 -0300

    create theme in tailwind

--- src/tailwind.config.mjs
+++ src/tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      "primary-Dark-Violet": "#2d2640",
+      "primary-Grayish-Blue": "#95a9c6",
+      "neutral-Very-Dark-Violet": "#2b272f",
+      "neutral-Dark-Grayish-Violet": "#837d87",
+      "neutral-Very-Light-Gray": "#fafafa",
+    },
+    fontFamily: {
+      headings: ["DM Serif Display", "sans-serif"],
+      body: ["Karla", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
